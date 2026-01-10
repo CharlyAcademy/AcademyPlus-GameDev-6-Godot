4160739c51749f08523182c529ccb9df003bf590
Carlos Chin <carlos.chindz@gmail.com>
2026-01-09 19:52:18 -0600

    Added platform and killzone

---
+++ primeros-pasos-con-godot/scripts/RULES.gd
@@ -0,0 +1,27 @@
+extends Node
+
+func _test1():
+	print("Hola mundo") #Bien
+
+#func _test2():
+#print("Hola mundo") #Mal
+
+#Identado despues de utilizar dos puntos
+func _test3():
+	print("Hola test 3")
+	if 5 < 3 :
+		print("No")
+		
+#Uso de parentesis
+func _test4():
+	print ("Ganaste")
+	
+#Uso de mayusculas y minusculas
+func _test5():
+	print("Ganaste")
+	#Print("Ganaste")
+	
+#Codigo limpio
+#func _test6():
+#	move_and_slide(velocity, Vector2.UP)
+	#move_and_slide(velocity,Vector2.UP)
---
+++ primeros-pasos-con-godot/scripts/killzone.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body):
+	timer.start()
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
